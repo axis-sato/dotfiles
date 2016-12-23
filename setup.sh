1ef3565a1b660c80a52936bc8971db0bc9f70e09
Masahiko Sato <sato_m@mediaxis.jp>
2016-12-23 22:31:34 +0900

    setup.shを修正

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-for file in `\find . -maxdepth 1 -name "\.*" ! -path .` ; do
-    ln -sf $file $Home/
-    echo $file
+for file in `\find . -maxdepth 1 -name "\.*" ! -path . ! -name ".git*"` ; do
+    ln -sf $PWD/$file $HOME/
+    echo "ln -sf $PWD/$file $HOME/"
+done
+
+zprezto_file_path="./.zprezto/runcoms"
+for file in `\find $zprezto_file_path -maxdepth 1 -type file ! -path . ! -name README.md` ; do
+    ln -sf $PWD/$file $HOME/.$(basename $file)
+    echo "ln -sf $PWD/$file $HOME/.$(basename $file)"
 done
