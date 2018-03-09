4d497c590a8af26ef5ae51d47078f8941d3db216
sato <sato_m@mediaxis.jp>
2018-03-09 23:37:25 +0900

    add output

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # link dotfiles
+echo "link dotfiles to $HOME"
 for file in `\find . -maxdepth 1 -name "\.*" ! -path . ! -name ".git*"` ; do
     ln -sf $PWD/$file $HOME/
     echo "ln -sf $PWD/$file $HOME/"
