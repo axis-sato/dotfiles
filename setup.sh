71b24afc92a504b3b60a14af6c39cbc9d9a9f825
sato <sato_m@mediaxis.jp>
2018-03-09 23:03:03 +0900

    remove zprezto

--- setup.sh
+++ setup.sh
@@ -6,13 +6,6 @@ for file in `\find . -maxdepth 1 -name "\.*" ! -path . ! -name ".git*"` ; do
     echo "ln -sf $PWD/$file $HOME/"
 done
 
-zprezto_file_path="./.zprezto/runcoms"
-for file in `\find $zprezto_file_path -maxdepth 1 -type file ! -path . ! -name README.md` ; do
-    ln -sf $PWD/$file $HOME/.$(basename $file)
-    echo "ln -sf $PWD/$file $HOME/.$(basename $file)"
-done
-
-
 # install dein.vim
 dein_path=$HOME/Desktop/dein
 mkdir $dein_path
