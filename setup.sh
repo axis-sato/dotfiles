dfc926c64b85c3cf89128b8f7aafb3d45ca21445
sato <sato_m@mediaxis.jp>
2018-03-09 23:36:20 +0900

    remove dein

--- setup.sh
+++ setup.sh
@@ -5,9 +5,3 @@ for file in `\find . -maxdepth 1 -name "\.*" ! -path . ! -name ".git*"` ; do
     ln -sf $PWD/$file $HOME/
     echo "ln -sf $PWD/$file $HOME/"
 done
-
-# install dein.vim
-dein_path=$HOME/Desktop/dein
-mkdir $dein_path
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ${dein_path}/installer.sh
-sh $dein_path/installer.sh $dein_path
