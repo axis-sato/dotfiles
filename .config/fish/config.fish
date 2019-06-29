5b7c477ffbc3b90b8432cc1b8f141071a2051db0
Masahiko Sato <sato_m@mediaxis.jp>
2019-06-29 17:53:11 +0900

    Edit fish settings

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+# Setup flutter
+if command -s flutter > /dev/null
+  set -x PATH (dirname (command -s flutter)) $PATH
+end
+
+# Setup swiftenv
+if command -s swiftenv > /dev/null
+  set -x SWIFTENV_ROOT $HOME/.swiftenv
+  set -x PATH $SWIFTENV_ROOT/bin $PATH
+  status --is-interactive; and source (swiftenv init -|psub)
+end
+
+# Setup asdf
+set -l asdf_fish_path /usr/local/opt/asdf/asdf.fish
+if test -f $asdf_fish_path
+  source $asdf_fish_path
+end
