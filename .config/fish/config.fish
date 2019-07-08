05cac1b6f546cb43a88b390fe450b426322c6261
sato <sato_m@mediaxis.jp>
2019-07-08 16:53:04 +0900

    set GOPATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,26 @@
+# Setup go
+set -l go_path $HOME/go
+if test -d $go_path
+    set -x GOPATH $go_path
+    set -x PATH $GOPATH $PATH
+end
+
 # Setup flutter
 set -l flutter_path $HOME/flutter/bin
 if test -d $flutter_path
-  set -x PATH $flutter_path $PATH
+    set -x PATH $flutter_path $PATH
 end
 
 # Setup swiftenv
-if command -s swiftenv > /dev/null
-  set -x SWIFTENV_ROOT $HOME/.swiftenv
-  set -x PATH $SWIFTENV_ROOT/bin $PATH
-  status --is-interactive; and source (swiftenv init -|psub)
+if command -s swiftenv >/dev/null
+    set -x SWIFTENV_ROOT $HOME/.swiftenv
+    set -x PATH $SWIFTENV_ROOT/bin $PATH
+    status --is-interactive
+    and source (swiftenv init -|psub)
 end
 
 # Setup asdf
 set -l asdf_fish_path /usr/local/opt/asdf/asdf.fish
 if test -f $asdf_fish_path
-  source $asdf_fish_path
+    source $asdf_fish_path
 end
